1704dc7c578899fe11b2a6968ca6d127bbe88720
medal17 <iyandaadeshina@gmail.com>
2022-05-22 18:28:12 +0100

    adds stuff

--- pages/splashScreen.js
+++ pages/splashScreen.js
@@ -1,23 +1,32 @@
 import { View, Text, Image, ActivityIndicator } from 'react-native'
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import tw from 'tailwind-react-native-classnames'
+
+import RoundedButton from '../components/button/RoundedButton'
 
 export default function SplashScreen({navigation}) {
 
-    useEffect(()=>{
+  const [load, setLoad] = useState(false)
+    
+  useEffect(()=>{
         setTimeout(
             function() {
             //  setisChecked(id)
             navigation.navigate('home')
+            setLoad(true)
             }, 200);
     },[])
   return (
     <View style={tw` h-full`}>
       <Image style={tw`h-24 m-auto`} resizeMode='contain' source={require('../images/Logo/Buk.jpg')}/>  
       {/* <Text>SplashScreen</Text> */}
-      <View style={tw`mx-auto mb-5`}>
+      { !load ?
+        <View style={tw`mx-auto mb-5`}>
         <ActivityIndicator style={tw`absolute bottom-0`} size={30} color='#0092DE' />
-      </View>
+      </View>:
+      <View style={tw`w-7/12 mx-auto mb-5`}>
+        <RoundedButton text='Next' pressed={()=>navigation.navigate('home')} />
+      </View>}
     </View>
   )
 }
